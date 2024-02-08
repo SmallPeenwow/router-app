4a8ea77978dceb4d652315658b1c576a05d72b80
SmallPeenwow <bowdencamryn@gmail.com>
2024-02-08 17:15:36 +0200

    7.3 Navigating To Routes Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
 // There is routes order. WildCard '**' should be at bottom of routing order
 const routes: Routes = [
   { path: '', component: FirstComponent },
   { path: 'second', component: SecondComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
